4f8084000defd38aef501f7fba2a0799663e7ab1
Ernie Shu <ernie.shu@intelliware.ca>
2016-06-13 16:29:30 -0400

    Fixing up build + run script to correspond with released version.

--- sample-buildAndRunScriptGenerator.sh
+++ sample-buildAndRunScriptGenerator.sh
@@ -1,2 +1,2 @@
 mvn clean install
-java -jar target/flywaydb-script-generator-0.9-SNAPSHOT.jar filesystem:src/test/resources/dbmigrations 1 1.001 migrationscript.sql ORACLE
+java -jar target/flywaydb-script-generator-0.9.jar filesystem:src/test/resources/dbmigrations 1 1.001 migrationscript.sql ORACLE
