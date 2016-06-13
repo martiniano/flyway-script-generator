c569f8c846970883873f0d2d1ee7601530f9c894
Ernie Shu <ernie.shu@intelliware.ca>
2016-06-13 16:34:49 -0400

    Expanding DB support to all DBs that Flyway supports.

--- src/main/java/com/iwd/schema/service/SupportedDBTypes.java
+++ src/main/java/com/iwd/schema/service/SupportedDBTypes.java
@@ -1,8 +1,17 @@
 package com.iwd.schema.service;
 
 import org.flywaydb.core.internal.dbsupport.DbSupport;
+import org.flywaydb.core.internal.dbsupport.db2.DB2DbSupport;
+import org.flywaydb.core.internal.dbsupport.db2zos.DB2zosDbSupport;
+import org.flywaydb.core.internal.dbsupport.h2.H2DbSupport;
+import org.flywaydb.core.internal.dbsupport.hsql.HsqlDbSupport;
+import org.flywaydb.core.internal.dbsupport.mysql.MySQLDbSupport;
 import org.flywaydb.core.internal.dbsupport.oracle.OracleDbSupport;
 import org.flywaydb.core.internal.dbsupport.postgresql.PostgreSQLDbSupport;
+import org.flywaydb.core.internal.dbsupport.redshift.RedshiftDbSupport;
+import org.flywaydb.core.internal.dbsupport.sqlite.SQLiteDbSupport;
+import org.flywaydb.core.internal.dbsupport.sqlserver.SQLServerDbSupport;
+import org.flywaydb.core.internal.dbsupport.vertica.VerticaDbSupport;
 
 /**
  * This Source Code Form is subject to the terms of the Mozilla Public License,
@@ -11,7 +20,11 @@ import org.flywaydb.core.internal.dbsupport.postgresql.PostgreSQLDbSupport;
  */
 public enum SupportedDBTypes {
 
-	ORACLE(new OracleDbSupport(null)), POSTGRESQL(new PostgreSQLDbSupport(null));
+	ORACLE(new OracleDbSupport(null)), POSTGRESQL(new PostgreSQLDbSupport(null)), DB2(new DB2DbSupport(null)), DB2zos(
+			new DB2zosDbSupport(null)), H2(new H2DbSupport(null)), Hsql(
+					new HsqlDbSupport(null)), MySQL(new MySQLDbSupport(null)), REDSHIFT(
+							new RedshiftDbSupport(null)), SQLITE(new SQLiteDbSupport(null)), SQLServer(
+									new SQLServerDbSupport(null)), VERTICA(new VerticaDbSupport(null));
 	private DbSupport dbSupport;
 
 	private SupportedDBTypes(DbSupport dbSupport) {
