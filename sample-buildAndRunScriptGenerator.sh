8d394afbcd03609b1f1009e9c149295ad801010f
Ernie Shu <ernie.shu@intelliware.com>
2016-06-15 10:35:11 -0400

    Updating run scripts to include passphrase for gpg.

--- sample-buildAndRunScriptGenerator.sh
+++ sample-buildAndRunScriptGenerator.sh
@@ -1,2 +1,2 @@
-mvn clean install
+mvn clean install -Dgpg.passphrase=somePassphraseHere
 java -jar target/flywaydb-script-generator-0.9.jar filesystem:src/test/resources/dbmigrations 1 1.001 migrationscript.sql ORACLE
